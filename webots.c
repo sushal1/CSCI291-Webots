40514429dff5b9469704985e0cd83e0101e6b139
sushal1 <92775491+sushal1@users.noreply.github.com>
2024-11-18 22:52:33 +0400

    Add files via upload

--- webots.c
+++ webots.c
@@ -1,5 +1,6 @@
 #include <webots/robot.h>
 #include <webots/motor.h>
+#include <webots/gps.h>
 #include <webots/distance_sensor.h>
 
 #include <stdio.h>
@@ -10,6 +11,10 @@
 
 int main(int argc, char **argv) {
   wb_robot_init();
+  
+  WbDeviceTag gps = wb_robot_get_device("gps");
+  wb_gps_enable(gps, TIME_STEP);
+   
   
   // initialising the motors
   WbDeviceTag left_motor = wb_robot_get_device("left wheel motor");
@@ -33,6 +38,10 @@ int main(int argc, char **argv) {
   double right_speed = MAX_SPEED;
 
   while (wb_robot_step(TIME_STEP) != -1) {
+  
+    const double *position = wb_gps_get_values(gps);
+    printf("Position: x = %f, y = %f, z = %f\n", position[0], position[1], position[2]);
+    
     bool left_wall = wb_distance_sensor_get_value(proximity_sensors[5]) > 100;
     bool left_corner = wb_distance_sensor_get_value(proximity_sensors[6]) > 100;
     bool front_wall = wb_distance_sensor_get_value(proximity_sensors[7]) > 100;
@@ -45,25 +54,75 @@ int main(int argc, char **argv) {
     printf("Left corner: %lf\n",leftcornersensor);
     printf("Front wall: %lf\n",frontwallsensor);
     
-    if (front_wall == true) {
-      left_speed = MAX_SPEED;
-      right_speed = -MAX_SPEED;
-      
-    } else {
+    //int found = 0;
+    //if ((position[0] == -1.11) && (position[2] == 0.08)) {
+      //if (found != 0) {
+        //printf("\noops\n");
+        //left_speed = 0;
+        //right_speed = 0;
+      //} else {
+        //found++;
+        //wall_detector(left_speed, right_speed, left_wall,front_wall, left_corner);
+      //}
+    //} else {
+      //wall_detector(left_speed, right_speed, left_wall, front_wall, left_corner);
+    //}
     
-      if (left_wall == true) {
-        left_speed = MAX_SPEED;
-        right_speed = MAX_SPEED;
-        
-      } else if (left_corner == true) {
-        left_speed = MAX_SPEED;
-        right_speed = MAX_SPEED/5;
-        
+    int found = 0;
+    if ((position[0] == 1.111802) && (position[2] == 0.044266)) {
+      if (found != 0) {
+        printf("oops\n");
+        left_speed = 0;
+        right_speed = 0;
       } else {
-        left_speed = MAX_SPEED/5;
-        right_speed = MAX_SPEED;
+        found++;
+        if (front_wall == true) {
+          left_speed = MAX_SPEED;
+          right_speed = -MAX_SPEED;
+          printf("Frontwall");
+        } else {
+          if (left_wall == true) {
+            left_speed = MAX_SPEED;
+            right_speed = MAX_SPEED;
+            printf("Leftwall");
+            
+          } else if (left_corner == true) {
+            left_speed = MAX_SPEED;
+            right_speed = MAX_SPEED/5;
+            printf("corner");
+            
+          } else {
+            left_speed = MAX_SPEED/5;
+            right_speed = MAX_SPEED;
+            printf("else");
+          }
+        }
+      }
+    } else {
+      if (front_wall == true) {
+        left_speed = MAX_SPEED;
+        right_speed = -MAX_SPEED;
+        printf("Frontwall");
+      } else {
+        if (left_wall == true) {
+          left_speed = MAX_SPEED;
+          right_speed = MAX_SPEED;
+          printf("Leftwall");
+          
+        } else if (left_corner == true) {
+          left_speed = MAX_SPEED;
+          right_speed = MAX_SPEED/5;
+          printf("corner");
+          
+        } else {
+          left_speed = MAX_SPEED/5;
+          right_speed = MAX_SPEED;
+          printf("else");
+        }
       }
     }
+    
+    //wall_detector(left_speed, right_speed, left_wall, front_wall, left_corner);
     
     wb_motor_set_velocity(left_motor,left_speed);
     wb_motor_set_velocity(right_motor,right_speed);
